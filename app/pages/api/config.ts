f488a8dcd629edca416fb30927b7713be06dc0be
Ryan Jandrick Obeles <51547772+rjbobeles@users.noreply.github.com>
2021-11-13 12:28:37 +0800

    Update config

--- app/pages/api/config.ts
+++ app/pages/api/config.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   if (req.method !== "GET") {
     res.status(405).send({
       status: res.statusCode,
@@ -11,6 +13,7 @@ export default function handler(req, res) {
     status: res.statusCode,
     config: {
       BASE_URL: process.env.BASE_URL ? process.env.BASE_URL : "NO_URL_SET",
+      SMART_ESCROW_ADDRESS: process.env.SMART_ESCROW_ADDRESS ? process.env.SMART_ESCROW_ADDRESS : "0x0",
     },
   });
 }
